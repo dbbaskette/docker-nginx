c690fd3612b0f4e65109938ef83c6b6111b58a63
Dan Baskette <dbbaskette@gmail.com>
2016-08-19 16:27:22 -0400

    Update run.sh

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
- 
+
 # From https://gist.github.com/xaviervia/6adea3ddba269cadb794
 
 env
 
+export ZEPPELIN_PORT_8080_TCP_ADDR=<INTERNALIP>
+export ZEPPELIN_PORT_8080_TCP_PORT=8080
+export ZEPPELIN_PORT_8081_TCP_ADDR=<INTERNALIP>
+export ZEPPELIN_PORT_8081_TCP_PORT=8081
+
 # Preprend the upstream configuration
-sed -i s/ZEPPELIN_PORT_8080_TCP_ADDR/${ZEPPELIN_PORT_8080_TCP_ADDR}/g /etc/nginx/nginx.conf 
+sed -i s/ZEPPELIN_PORT_8080_TCP_ADDR/${ZEPPELIN_PORT_8080_TCP_ADDR}/g /etc/nginx/nginx.conf
 sed -i s/ZEPPELIN_PORT_8080_TCP_PORT/${ZEPPELIN_PORT_8080_TCP_PORT}/g /etc/nginx/nginx.conf
 sed -i s/ZEPPELIN_PORT_8081_TCP_ADDR/${ZEPPELIN_PORT_8081_TCP_ADDR}/g /etc/nginx/nginx.conf
 sed -i s/ZEPPELIN_PORT_8081_TCP_PORT/${ZEPPELIN_PORT_8081_TCP_PORT}/g /etc/nginx/nginx.conf
@@ -15,6 +20,6 @@ cat /etc/nginx/nginx.conf
 
 echo "Configuring zeppelin user and password..."
 htpasswd -cb /etc/nginx/.htpasswd $ZEPPELIN_USER $ZEPPELIN_PASSWORD
- 
+
 # Start nginx
 nginx
